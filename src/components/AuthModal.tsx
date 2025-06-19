0351abd95ced89cd92d0853b5ac291b80b1a0491
Shaik-Policharla-Amrin <policharlaamrin@gmail.com>
2025-06-19 17:24:11 +0530

    Updated AuthModal.tsx

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -92,7 +92,7 @@ export default function AuthModal({ isOpen, onClose, mode, onModeChange }: AuthM
               Email Address
             </label>
             <div className="relative">
-              <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
+              <Mail className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#0071ce] focus:border-black text-black" />
               <input
                 type="email"
                 value={email}
